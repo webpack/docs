2796e1ad4e10f30dce6c240eade65f0803969e11
Tobias Koppers <tobias.koppers@googlemail.com>
2014-06-27 15:07:01 +0200

    Publish

--- tutorials/migrate-to-webpack/other-resources/bundle.js
+++ tutorials/migrate-to-webpack/other-resources/bundle.js
@@ -1,42 +1,41 @@
 /******/ (function(modules) { // webpackBootstrap
-/******/ 	
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/ 	
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/ 		
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-/******/ 		
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/ 		
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-/******/ 		
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/ 	
-/******/ 	
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/ 	
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/ 	
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/ 	
-/******/ 	
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -126,17 +125,18 @@
 		}
 		var styleElement = document.createElement("style");
 		styleElement.type = "text/css";
+		var head = document.getElementsByTagName("head")[0];
+		head.appendChild(styleElement);
 		if (styleElement.styleSheet) {
 			styleElement.styleSheet.cssText = cssCode;
 		} else {
 			styleElement.appendChild(document.createTextNode(cssCode));
 		}
-		var head = document.getElementsByTagName("head")[0];
-		head.appendChild(styleElement);
 		return function() {
 			head.removeChild(styleElement);
 		};
 	}
+
 
 /***/ },
 /* 7 */
