a91f4040543ec4e5565d054380d5b9dd5c51d529
Tobias Koppers <tobias.koppers@googlemail.com>
2014-07-27 19:17:35 +0200

    Publish

--- tutorials/migrate-to-webpack/templates/bundle.js
+++ tutorials/migrate-to-webpack/templates/bundle.js
@@ -69,11 +69,11 @@
 
 	var $, template;
 
-	__webpack_require__(5);
+	__webpack_require__(4);
 
 	$ = __webpack_require__(3);
 
-	template = __webpack_require__(4);
+	template = __webpack_require__(6);
 
 	exports.render = function(model) {
 	  return $("body").html(template({
@@ -99,37 +99,35 @@
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var jade = __webpack_require__(10);
-
-	module.exports = function anonymous(locals, attrs, escape, rethrow, merge) {
-	attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
-	var buf = [];
-	with (locals || {}) {
-	var interp;
-	buf.push('<h1>Hello World.</h1><p>Your User-Agent is: ' + escape((interp = model) == null ? '' : interp) + '</p>');
-	}
-	return buf.join("");
-	}
-
-/***/ },
-/* 5 */
-/***/ function(module, exports, __webpack_require__) {
-
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 	var dispose = __webpack_require__(7)
 		// The css code:
-		(__webpack_require__(6))
+		(__webpack_require__(5))
 	if(false) {
 		module.hot.accept();
 		module.hot.dispose(dispose);
 	}
 
 /***/ },
-/* 6 */
+/* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports =
 		"body {\n  background: #eee;\n}\nbody p {\n  border: 1px solid #333;\n  border-radius: 3px;\n}\n";
+
+/***/ },
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var jade = __webpack_require__(10);
+
+	module.exports = function template(locals) {
+	var buf = [];
+	var jade_mixins = {};
+	var jade_interp;
+	var locals_ = (locals || {}),model = locals_.model;
+	buf.push("<h1>Hello World.</h1><p>Your User-Agent is: " + (jade.escape((jade_interp = model) == null ? '' : jade_interp)) + "</p>");;return buf.join("");
+	}
 
 /***/ },
 /* 7 */
@@ -186,38 +184,7 @@
 /* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
-	
-	/*!
-	 * Jade - runtime
-	 * Copyright(c) 2010 TJ Holowaychuk <tj@vision-media.ca>
-	 * MIT Licensed
-	 */
-
-	/**
-	 * Lame Array.isArray() polyfill for now.
-	 */
-
-	if (!Array.isArray) {
-	  Array.isArray = function(arr){
-	    return '[object Array]' == Object.prototype.toString.call(arr);
-	  };
-	}
-
-	/**
-	 * Lame Object.keys() polyfill for now.
-	 */
-
-	if (!Object.keys) {
-	  Object.keys = function(obj){
-	    var arr = [];
-	    for (var key in obj) {
-	      if (obj.hasOwnProperty(key)) {
-	        arr.push(key);
-	      }
-	    }
-	    return arr;
-	  }
-	}
+	'use strict';
 
 	/**
 	 * Merge two attribute objects giving precedence
@@ -232,6 +199,13 @@
 	 */
 
 	exports.merge = function merge(a, b) {
+	  if (arguments.length === 1) {
+	    var attrs = a[0];
+	    for (var i = 1; i < a.length; i++) {
+	      attrs = merge(attrs, a[i]);
+	    }
+	    return attrs;
+	  }
 	  var ac = a['class'];
 	  var bc = b['class'];
 
@@ -240,9 +214,7 @@
 	    bc = bc || [];
 	    if (!Array.isArray(ac)) ac = [ac];
 	    if (!Array.isArray(bc)) bc = [bc];
-	    ac = ac.filter(nulls);
-	    bc = bc.filter(nulls);
-	    a['class'] = ac.concat(bc).join(' ');
+	    a['class'] = ac.concat(bc).filter(nulls);
 	  }
 
 	  for (var key in b) {
@@ -257,14 +229,74 @@
 	/**
 	 * Filter null `val`s.
 	 *
-	 * @param {Mixed} val
-	 * @return {Mixed}
+	 * @param {*} val
+	 * @return {Boolean}
 	 * @api private
 	 */
 
 	function nulls(val) {
-	  return val != null;
+	  return val != null && val !== '';
 	}
+
+	/**
+	 * join array as classes.
+	 *
+	 * @param {*} val
+	 * @return {String}
+	 */
+	exports.joinClasses = joinClasses;
+	function joinClasses(val) {
+	  return Array.isArray(val) ? val.map(joinClasses).filter(nulls).join(' ') : val;
+	}
+
+	/**
+	 * Render the given classes.
+	 *
+	 * @param {Array} classes
+	 * @param {Array.<Boolean>} escaped
+	 * @return {String}
+	 */
+	exports.cls = function cls(classes, escaped) {
+	  var buf = [];
+	  for (var i = 0; i < classes.length; i++) {
+	    if (escaped && escaped[i]) {
+	      buf.push(exports.escape(joinClasses([classes[i]])));
+	    } else {
+	      buf.push(joinClasses(classes[i]));
+	    }
+	  }
+	  var text = joinClasses(buf);
+	  if (text.length) {
+	    return ' class="' + text + '"';
+	  } else {
+	    return '';
+	  }
+	};
+
+	/**
+	 * Render the given attribute.
+	 *
+	 * @param {String} key
+	 * @param {String} val
+	 * @param {Boolean} escaped
+	 * @param {Boolean} terse
+	 * @return {String}
+	 */
+	exports.attr = function attr(key, val, escaped, terse) {
+	  if ('boolean' == typeof val || null == val) {
+	    if (val) {
+	      return ' ' + (terse ? key : key + '="' + key + '"');
+	    } else {
+	      return '';
+	    }
+	  } else if (0 == key.indexOf('data') && 'string' != typeof val) {
+	    return ' ' + key + "='" + JSON.stringify(val).replace(/'/g, '&apos;') + "'";
+	  } else if (escaped) {
+	    return ' ' + key + '="' + exports.escape(val) + '"';
+	  } else {
+	    return ' ' + key + '="' + val + '"';
+	  }
+	};
 
 	/**
 	 * Render the given attributes object.
@@ -272,42 +304,28 @@
 	 * @param {Object} obj
 	 * @param {Object} escaped
 	 * @return {String}
-	 * @api private
 	 */
+	exports.attrs = function attrs(obj, terse){
+	  var buf = [];
 
-	exports.attrs = function attrs(obj, escaped){
-	  var buf = []
-	    , terse = obj.terse;
+	  var keys = Object.keys(obj);
 
-	  delete obj.terse;
-	  var keys = Object.keys(obj)
-	    , len = keys.length;
-
-	  if (len) {
-	    buf.push('');
-	    for (var i = 0; i < len; ++i) {
+	  if (keys.length) {
+	    for (var i = 0; i < keys.length; ++i) {
 	      var key = keys[i]
 	        , val = obj[key];
 
-	      if ('boolean' == typeof val || null == val) {
-	        if (val) {
-	          terse
-	            ? buf.push(key)
-	            : buf.push(key + '="' + key + '"');
+	      if ('class' == key) {
+	        if (val = joinClasses(val)) {
+	          buf.push(' ' + key + '="' + val + '"');
 	        }
-	      } else if (0 == key.indexOf('data') && 'string' != typeof val) {
-	        buf.push(key + "='" + JSON.stringify(val) + "'");
-	      } else if ('class' == key && Array.isArray(val)) {
-	        buf.push(key + '="' + exports.escape(val.join(' ')) + '"');
-	      } else if (escaped && escaped[key]) {
-	        buf.push(key + '="' + exports.escape(val) + '"');
 	      } else {
-	        buf.push(key + '="' + val + '"');
+	        buf.push(exports.attr(key, val, false, terse));
 	      }
 	    }
 	  }
 
-	  return buf.join(' ');
+	  return buf.join('');
 	};
 
 	/**
@@ -319,11 +337,13 @@
 	 */
 
 	exports.escape = function escape(html){
-	  return String(html)
-	    .replace(/&(?!(\w+|\#\d+);)/g, '&amp;')
+	  var result = String(html)
+	    .replace(/&/g, '&amp;')
 	    .replace(/</g, '&lt;')
 	    .replace(/>/g, '&gt;')
 	    .replace(/"/g, '&quot;');
+	  if (result === '' + html) return html;
+	  else return result;
 	};
 
 	/**
@@ -336,11 +356,18 @@
 	 * @api private
 	 */
 
-	exports.rethrow = function rethrow(err, filename, lineno){
-	  if (!filename) throw err;
-
+	exports.rethrow = function rethrow(err, filename, lineno, str){
+	  if (!(err instanceof Error)) throw err;
+	  if ((typeof window != 'undefined' || !filename) && !str) {
+	    err.message += ' on line ' + lineno;
+	    throw err;
+	  }
+	  try {
+	    str =  str || __webpack_require__(11).readFileSync(filename, 'utf8')
+	  } catch (ex) {
+	    rethrow(err, null, lineno)
+	  }
 	  var context = 3
-	    , str = __webpack_require__(11).readFileSync(filename, 'utf8')
 	    , lines = str.split('\n')
 	    , start = Math.max(lineno - context, 0)
 	    , end = Math.min(lines.length, lineno + context);
@@ -366,9 +393,7 @@
 /* 11 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var files = {};
-	exports.setFile = function(filename, content) { files[filename] = content; }
-	exports.readFileSync = function(filename) { return files[filename] || ""; }
+
 
 /***/ }
 /******/ ])
