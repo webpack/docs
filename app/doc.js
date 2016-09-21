b3037388844744665504a41af014359baa69eeeb
Josh Dzielak <jdzielak@gmail.com>
2016-09-21 14:57:43 -0700

    Add DocSearch libraries and search box

--- app/doc.js
+++ app/doc.js
@@ -1,6 +1,7 @@
 require("./css/doc.styl");
 require("./googleAnalytics");
 require("./disqus");
+require("./docsearch");
 
 require("./onContentLoaded")(function(event) {
 	require("./bindToIntraLinks");
